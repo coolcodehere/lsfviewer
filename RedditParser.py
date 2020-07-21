bcda42a892e9abe9913e3f37e5dba6adf4a94801
Will Walter <wwalte1@ilstu.edu>
2020-07-20 22:02:32 -0500

    initial commit

---
+++ RedditParser.py
@@ -0,0 +1,19 @@
+#! python3
+import praw
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+reddit = config["Reddit"]
+reddit = praw.Reddit(client_id = reddit["clientID"], \
+                     client_secret = reddit["clientSecret"], \
+                     user_agent = "LSF Scraper", \
+                     username = reddit["redditAccountUsername"], \
+                     password = reddit["redditAccountPassword"])
+subreddit = reddit.subreddit("LivestreamFail")
+
+def getDailyTopPosts(limit):
+  postUrls = []
+  for post in subreddit.top(time_filter="day", limit=limit):
+    postUrls.append(post.url)
+  return postUrls
